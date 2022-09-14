10e93491affda6d99e459030cccff3987e9531d7
himanshu60 <baliyanh10@gmail.com>
2022-09-14 16:07:43 +0530

    code-fix

---
+++ prime_palindrome.js
@@ -0,0 +1,26 @@
+// prime Number
+
+num=15;
+count=0;
+    for(let i=1;i<=num;i++){
+        if(num%i===0){
+            count++;
+        }}
+    if(count==2){
+            
+        }else{
+            
+}
+
+// check palindrome
+
+str="naman";
+let bag="";
+  for(let i=N-1; i>=0;i--){
+      bag=bag+str[i];
+    
+}if(str==bag){
+    
+}else{
+    
+}
